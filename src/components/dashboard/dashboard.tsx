8e6374f58f63b6f1f37212692867e250ebfeddce
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2024-07-02 14:39:26 +0700

    add button and icon

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -4,11 +4,16 @@ import { CSSProperties, useEffect, useState} from "react"
 import styled from 'styled-components';
 import classes from './dashboard.module.css'
 import jwt,{ JwtPayload } from 'jsonwebtoken'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare,faTrash } from '@fortawesome/free-solid-svg-icons';
 interface DecodedToken extends JwtPayload {
     email: string;
     role: string;
   }
+
+interface Employee{
+
+}
 
 export default function DashboardPage(){
     const router = useRouter();
@@ -41,42 +46,77 @@ export default function DashboardPage(){
         }
     return (
         <div className={classes.container}>
-        <div className={classes.header}>
-            <h1>My Application</h1>
-        </div>
             <div className={classes.navbar}>
-               <ul>
-                <li><a href="/dashboard">Trang chủ</a></li>
-                <li><a href="#">Quản lý tài khoản</a></li>
-                <li><a href="#">Quản lý nhân viên</a></li>
-                <li><a href="#">Quản lý lịch làm việc</a></li>
-                <li><a href="#">Quản lý chấm công</a></li>
-                <li><a href="#">Quản lý tính lương</a></li>
-               </ul>
+                <ul>
+                    <li><a href="/dashboard">Trang chủ</a></li>
+                    <li><a href="#">Quản lý tài khoản</a></li>
+                    <li><a href="#">Quản lý nhân viên</a></li>
+                    <li><a href="#">Quản lý lịch làm việc</a></li>
+                    <li><a href="#">Quản lý chấm công</a></li>
+                    <li><a href="#">Quản lý tính lương</a></li>
+                </ul>
             </div>
+            <div className={classes.maincontainer}>
+                <div className={classes.header}>
+                    <h1>My Application</h1>
+                </div>
 
-            <div className={classes.article}>
-               <table>
-                    <thead>
-                        <th>Mã nhân viên</th>
-                        <th>Họ nhân viên</th>
-                        <th>Tên nhân viên</th>
-                        <th>Địa chỉ</th>
-                        <th>Số điện thoại</th>
-                        <th>CMND</th>
-                        <th>Email</th>
-                    </thead>
+                <div className={classes.article}>
+                    <table>
+                        <thead>
+                            <th>Mã nhân viên</th>
+                            <th>Họ nhân viên</th>
+                            <th>Tên nhân viên</th>
+                            <th>Giới tính</th>
+                            <th>Địa chỉ</th>
+                            <th>Số điện thoại</th>
+                            <th>CMND</th>
+                            <th>Email</th>
+                            <th>Ngày làm việc</th>
+                            <th>Bằng cấp</th>
+                            <th>Trạng thái</th>
+                            <th>Hành động</th>
+                        </thead>
 
-                    <tbody>
-                        <td>NV001</td>
-                        <td>Nguyễn Văn</td>
-                        <td>Trường</td>
-                        <td>60/1/2</td>
-                        <td>08900809890</td>
-                        <td>012313123123</td>
-                        <td>trerq@gmail.com</td>
-                    </tbody>
-               </table>
+                        <tbody>
+                            <tr>
+                            <td>NV001</td>
+                            <td>Nguyễn Văn</td>
+                            <td>Trường</td>
+                            <td>Nam</td>
+                            <td>60/1/2</td>
+                            <td>08900809890</td>
+                            <td>012313123123</td>
+                            <td>trerq@gmail.com</td>
+                            <td>2024-07-02</td>
+                            <td>Đại học</td>
+                            <td>Đang làm việc</td>
+                            <td>
+                            <button>Cập nhật <FontAwesomeIcon icon={faPenToSquare} /></button>
+                                <button>Xóa <FontAwesomeIcon icon={faTrash} /></button>
+                            </td>
+                            </tr>
+
+                            <tr>
+                            <td>NV001</td>
+                            <td>Nguyễn Văn</td>
+                            <td>Trường</td>
+                            <td>Nam</td>
+                            <td>60/1/2</td>
+                            <td>08900809890</td>
+                            <td>012313123123</td>
+                            <td>trerq@gmail.com</td>
+                            <td>2024-07-02</td>
+                            <td>Đại học</td>
+                            <td>Đang làm việc</td>
+                            <td>
+                                <button>Cập nhật <FontAwesomeIcon icon={faPenToSquare} /></button>
+                                <button>Xóa <FontAwesomeIcon icon={faTrash} /></button>
+                            </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
