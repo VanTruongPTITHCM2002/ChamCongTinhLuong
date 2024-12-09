d24080e2b1da6612daf59305c06438f3f342284b
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2024-12-09 18:31:33 +0700

    sua lai ti

--- src/app/[id]/attendance/page.tsx
+++ src/app/[id]/attendance/page.tsx
@@ -3,6 +3,10 @@ import UserContainer from "@/components/user/container/container";
 
 import UserNavbar from "@/components/user/navbar/navbar";
 
+export async function generateStaticParams() {
+    const ids = ["1", "2", "3"]; // Thay thế danh sách này bằng dữ liệu thực tế
+    return ids.map((id) => ({ id }));
+  }
 export default function Page(){
     
     return(
--- src/app/[id]/profile/page.tsx
+++ src/app/[id]/profile/page.tsx
@@ -6,6 +6,10 @@ import UserProfile from "@/components/user/profile/profile";
 import { useRouter } from "next/navigation";
 
 
+export async function generateStaticParams() {
+  const ids = ["1", "2", "3"]; // Thay thế danh sách này bằng dữ liệu thực tế
+  return ids.map((id) => ({ id }));
+}
 export default function Page(){
 
     return(
