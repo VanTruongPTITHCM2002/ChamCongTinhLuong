8c8a379fabe7553198910546986a5654aebff864
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2024-12-09 18:28:24 +0700

    sua lai

--- src/app/[id]/changepassword/page.tsx
+++ src/app/[id]/changepassword/page.tsx
@@ -4,15 +4,18 @@ import UserContainer from "@/components/user/container/container";
 import UserHeader from "@/components/user/header/header";
 import UserNavbar from "@/components/user/navbar/navbar";
 
-export default function Page(){
-    
-    return(
-        <div>
-         <UserNavbar currentPath = "contract"/>
-         <UserContainer>
-            <UserChangePassword/>
+export async function generateStaticParams() {
+  const ids = ["1", "2", "3"]; // Thay thế danh sách này bằng dữ liệu thực tế
+  return ids.map((id) => ({ id }));
+}
+
+export default function Page({ params }: { params: { id: string } }) {
+  return (
+      <div>
+          <UserNavbar currentPath="contract" />
+          <UserContainer>
+              <UserChangePassword />
           </UserContainer>
-         
-        </div>
-    )
+      </div>
+  );
 }
